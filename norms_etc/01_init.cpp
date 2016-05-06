d75670704cd9875b2ac6e9c21a8610171bae5baa
Robert Sawko <RSawko@uk.ibm.com>
2016-05-06 21:28:44 +0100

    Add a really simple norm example

---
+++ norms_etc/01_init.cpp
@@ -0,0 +1,17 @@
+#include <Tpetra_DefaultPlatform.hpp>
+
+int
+main (int argc, char *argv[])
+{
+  using Teuchos::rcp;
+  using Teuchos::Comm;
+  using Teuchos::SerialComm;
+  Teuchos::RCP<const Comm<int>> comm = rcp(new SerialComm<int>());
+
+  const int myRank = comm -> getRank ();
+  const int numProcs = comm -> getSize ();
+  if (myRank == 0) {
+      std::cout << "Total number of processes: " << numProcs << std::endl;
+  }
+  return 0;
+}
---
+++ norms_etc/02_map+vector.cpp
@@ -0,0 +1,28 @@
+#include <Tpetra_Map.hpp>
+#include <Tpetra_Vector.hpp>
+#include <Teuchos_RCP.hpp>
+
+int
+main (int argc, char *argv[])
+{
+  using Teuchos::RCP;
+  using Teuchos::rcp;
+  using Teuchos::Comm;
+  using Teuchos::SerialComm;
+  RCP<const Comm<int>> comm = rcp(new SerialComm<int>());
+
+  // What is this lot?
+  typedef Tpetra::Vector<> vector_type;
+  typedef Tpetra::Vector<>::global_ordinal_type global_ordinal_type;
+  typedef Tpetra::Map<> map_type;
+
+  const global_ordinal_type indexBase = 0;
+  RCP<const map_type> contigMap =
+      rcp (new map_type (2, indexBase, comm));
+  vector_type x (contigMap);
+  x.putScalar (1.0);
+  std::cout<< "Norm of x (all entries are 1.0): "
+      << x.norm2 () << std::endl;
+
+  return 0;
+}
